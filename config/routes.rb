fc8a7265dc119e80b4010f4bc340f51dd9c146dc
ben7th <ben7th@sina.com>
2012-03-16 18:46:59 +0800

    init

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+TodoApp::Application.routes.draw do  
+  # -- 用户登录认证相关 --
+  root :to=>"index#index"
+  
+  get  '/login'  => 'sessions#new'
+  post '/login'  => 'sessions#create'
+  get  '/logout' => 'sessions#destroy'
+  
+  get  '/signup'        => 'signup#form'
+  post '/signup_submit' => 'signup#form_submit'
+  
+  # -- 以下可以自由添加其他 routes 配置项
+  
+end
